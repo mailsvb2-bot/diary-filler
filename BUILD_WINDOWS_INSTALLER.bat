b5608a41f6ebf64cf081e41adc8af73006fc6d26
BusinesMetro <mailsvb2@gmail.com>
2026-09-18 15:44:54 +0300

    release: build v1.4.16 installer

--- BUILD_WINDOWS_INSTALLER.bat
+++ BUILD_WINDOWS_INSTALLER.bat
@@ -20,13 +20,13 @@ if not defined ISCC (
   exit /b 1
 )
 
-"%ISCC%" /DMyAppVersion=1.4.15 "installer\MedicalDiaryAutofill.iss"
+"%ISCC%" /DMyAppVersion=1.4.16 "installer\MedicalDiaryAutofill.iss"
 if errorlevel 1 exit /b 1
 
-if not exist "dist\MedicalDiaryAutofill-Setup-1.4.15.exe" (
+if not exist "dist\MedicalDiaryAutofill-Setup-1.4.16.exe" (
   echo [ОШИБКА] Installer не создан.
   exit /b 1
 )
 
-echo ГОТОВО: dist\MedicalDiaryAutofill-Setup-1.4.15.exe
+echo ГОТОВО: dist\MedicalDiaryAutofill-Setup-1.4.16.exe
 exit /b 0
